c0d4846f875211ec09e78ad90d126b480eb65984
sandhyag-4 <sandhyaggg2005@gmail.com>
2026-02-26 13:00:14 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,7 @@
-public class{
-  public static void main(String args[]){
-    System.out.println("HELLO WORLD");}}
+public class
+{
+  public static void main(String args[])
+  {
+    System.out.println("HELLO WORLD");
+  }
+}
